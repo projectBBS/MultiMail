1ed76744383cd4a1cd6f06cc711011a9f5d9206e
projectBBS <evanlimanto@gmail.com>
2012-03-19 11:23:30 +0700

    Project Files

---
+++ src/MultiMail/HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author APPLE
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
